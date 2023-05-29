c887f3cd18d8e537e25a13bdd7a72c3531caf487
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-29 08:40:32 -0300

    preparando tela Cover

---
+++ src/components/Cover/index.tsx
@@ -0,0 +1,12 @@
+export const Cover = () => {
+    return(
+        <section>
+             <article>
+                <h1>ToDo List</h1>
+             </article>
+             <article>
+                <img src="" alt="" />
+             </article>
+        </section>
+    )
+}
