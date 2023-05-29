0b08bc30c015bd1b005e963cb6e05cc5c10e78cb
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-29 09:43:08 -0300

    terminando estilização 2

--- src/pages/Cover/index.tsx
+++ src/pages/Cover/index.tsx
@@ -1,16 +1,19 @@
 import styles from './styles.module.css'
 import capa from '../../assets/capa.svg'
+import { Link } from 'react-router-dom'
 
 export const Cover = () => {
-    return(
+    return (
         <section className={styles.container}>
-             <article className={styles.text}>
+            <article className={styles.text}>
                 <h1>ToDo List</h1>
                 <p>a</p>
-             </article>
-             <article className={styles.image}>
-                <img src={capa} alt="" />
-             </article>
+            </article>
+            <article >
+                <Link className={styles.conteiner_Image} to={'/To-Do'}>
+                    <img className={styles.image} src={capa} alt="" />
+                </Link>
+            </article>
         </section>
     )
 }
