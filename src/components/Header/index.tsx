2c64999a1fba2f2cb6e154f76142ce8bac4e9f70
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-05-29 08:16:06 -0300

    estilizando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ export const Header = () => {
     return (
         <header className={styles.header}>
             <img className={styles.img} src={logo} alt="Logo principal do sistema" />
+            <span>salve</span>
         </header>
     )
 }
